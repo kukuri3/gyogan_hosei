a3ab5284009a0381cc8405569b4928fd4c47b461
mima <kukuri3@gmail.com>
2015-04-06 09:00:52 +0900

    first commit

---
+++ gyogan_hosei/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(159, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(159, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 237);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(317, 253);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 57);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "効果を試す";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(63, 167);
+            this.trackBar1.Maximum = 2000;
+            this.trackBar1.Minimum = 800;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 42);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 800;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(196, 167);
+            this.trackBar2.Maximum = 2000;
+            this.trackBar2.Minimum = 800;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 42);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Value = 800;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 215);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "保存する";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "radius";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(194, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "distance";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 502);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ gyogan_hosei/Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Settings appSettings;   //アプリの設定パラメータなど
+        string gSettingFn;  //設定ファイルの名前
+
+        //アプリの設定保存用クラス
+        public class Settings
+        {
+            public int r;   //投影半径
+            public int d;   //投影距離
+            public bool save_check; //保存するかチェック
+            public Settings()
+            {
+                r = 1700;
+                d = 1200;
+                save_check = false;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.AllowDrop = true;  //フォームがドラッグアンドドロップを受けられるようにする
+            timer1.Enabled = true;
+
+            appSettings = new Settings();
+            gSettingFn = "setting.xml";
+
+            //＜XMLファイルから設定を読み込む＞
+
+            //XmlSerializerオブジェクトの作成
+            System.Xml.Serialization.XmlSerializer serializer2 =
+                new System.Xml.Serialization.XmlSerializer(typeof(Settings));
+            //ファイルを開く
+            System.IO.StreamReader sr = new System.IO.StreamReader(
+                gSettingFn, new System.Text.UTF8Encoding(false));
+            //XMLファイルから読み込み、逆シリアル化する
+            appSettings =
+                (Settings)serializer2.Deserialize(sr);
+            //閉じる
+            sr.Close();
+
+            //設定を回復する
+            trackBar1.Value = appSettings.r;
+            trackBar2.Value=appSettings.d;
+            checkBox1.Checked = appSettings.save_check;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            //コントロール内にドラッグされたとき実行される
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                //ドラッグされたデータ形式を調べ、ファイルのときはコピーとする
+                e.Effect = DragDropEffects.Copy;
+            else
+                //ファイル以外は受け付けない
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            //コントロール内にドロップされたとき実行される
+            //ドロップされたすべてのファイル名を取得する
+            string[] fileName =
+                (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //ListBoxに追加する
+            //listBox1.Items.AddRange(fileName);
+            
+            for (int i = 0; i < fileName.Length; i++)
+            {
+                string fn = fileName[i];
+                xLog(fn + "を処理中..");
+                if (System.IO.Path.GetExtension(fn).Equals(".jpg")||System.IO.Path.GetExtension(fn).Equals(".JPG"))
+                {
+                    //pictureBox1.ImageLocation = fn;
+                    pictureBox1.Image = new Bitmap(fn); //picturebox1にファイルから読み込む
+                    pictureBox1.Refresh();
+
+                    //Exif情報を列挙する
+                    
+                    foreach (System.Drawing.Imaging.PropertyItem item in pictureBox1.Image.PropertyItems)
+                    {
+                        //データの型を判断
+                        if (item.Type == 2)
+                        {
+                            //ASCII文字の場合は、文字列に変換する
+                            string val = System.Text.Encoding.ASCII.GetString(item.Value);
+                            val = val.Trim(new char[] { '\0' });
+                            //表示する
+                            xLog("id:"+item.Id+" type:"+item.Type+" val"+ val);
+                        }
+                        else
+                        {
+                            //表示する
+                            xLog("id:" + item.Id + " type:" + item.Type + " len:" + item.Len);
+                        }
+                    }
+
+
+                    Bitmap bitmap = new Bitmap(pictureBox1.Image);  //picturebox1から画像を読み込む
+                    Bitmap bitmap2 = new Bitmap(bitmap.Width,bitmap.Height);
+//                    xImageProcess(bitmap, bitmap2);
+                    xImageProcessGyogan(bitmap, bitmap2);   //画像処理をして、その結果がpicturebox2に入る
+
+                    pictureBox2.Refresh();
+                    //pictureBox2.Image = bitmap2;
+                    if (checkBox1.Checked == true)
+                    {    //保存する場合
+                        string dir = System.IO.Path.GetDirectoryName(fn);
+                        string filename = System.IO.Path.GetFileNameWithoutExtension(fn);
+                        string ext = System.IO.Path.GetExtension(fn);
+                        string fn2 = System.IO.Path.Combine(dir, filename + "_hosei"+ext);
+                        /*
+                        //JPEGの品質を設定
+                        System.Drawing.Imaging.EncoderParameters eps = new System.Drawing.Imaging.EncoderParameters(1);
+                        //品質を指定
+                        System.Drawing.Imaging.EncoderParameter ep =
+                            new System.Drawing.Imaging.EncoderParameter(
+                            System.Drawing.Imaging.Encoder.Quality, (long)90);
+                        //EncoderParametersにセットする
+                        eps.Param[0] = ep;
+
+                        //イメージエンコーダに関する情報を取得する
+                        System.Drawing.Imaging.ImageCodecInfo ici = GetEncoderInfo("image/jpeg");
+                        */
+
+                        //exif情報をコピーする
+                        foreach (System.Drawing.Imaging.PropertyItem item in pictureBox1.Image.PropertyItems)
+                        {
+                            bitmap2.SetPropertyItem(item);
+                        }
+                        //保存する
+                        bitmap2.Save(fn2, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+
+                    }
+                }
+            }
+
+        }
+        //MimeTypeで指定されたImageCodecInfoを探して返す
+        private static System.Drawing.Imaging.ImageCodecInfo
+            GetEncoderInfo(string mineType)
+        {
+            //GDI+ に組み込まれたイメージ エンコーダに関する情報をすべて取得
+            System.Drawing.Imaging.ImageCodecInfo[] encs =
+                System.Drawing.Imaging.ImageCodecInfo.GetImageEncoders();
+            //指定されたMimeTypeを探して見つかれば返す
+            foreach (System.Drawing.Imaging.ImageCodecInfo enc in encs)
+            {
+                if (enc.MimeType == mineType)
+                {
+                    return enc;
+                }
+            }
+            return null;
+        }
+
+        //ImageFormatで指定されたImageCodecInfoを探して返す
+        private static System.Drawing.Imaging.ImageCodecInfo
+            GetEncoderInfo(System.Drawing.Imaging.ImageFormat f)
+        {
+            System.Drawing.Imaging.ImageCodecInfo[] encs =
+                System.Drawing.Imaging.ImageCodecInfo.GetImageEncoders();
+            foreach (System.Drawing.Imaging.ImageCodecInfo enc in encs)
+            {
+                if (enc.FormatID == f.Guid)
+                {
+                    return enc;
+                }
+            }
+            return null;
+        }
+        private void xImageProcess(Bitmap srcimg,Bitmap dstimg)
+        {
+            //単純に拡大するだけバイリニア法で。
+            int w=srcimg.Width;
+            int h=srcimg.Height;
+
+            //srcimgに対して処理をして、picturebox2のbmpに格納する。
+
+//            BitmapData data = srcimg.LockBits(new Rectangle(0, 0, srcimg.Width, srcimg.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            BitmapData data = srcimg.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData data2 = dstimg.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            byte[] buf = new byte[w * h * 3];    //RGBバッファを確保
+            byte[] buf2 = new byte[w * h * 3];    //RGBバッファを確保
+            Marshal.Copy(data.Scan0, buf, 0, buf.Length);   //bufに画像をコピー
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+//                    Color color = xGetPixel(x, y, w, h, buf);
+
+                    Color color = BiLinearInterpolation(((double)x)/20, ((double)y)/20, w, h, buf);
+                    Color color2 = new Color();
+                    color2 = color;
+                    xSetPixel(x, y, color2, w, h, buf2);
+                }
+            }
+            
+  
+            Marshal.Copy(buf2, 0, data2.Scan0, buf2.Length);
+            srcimg.UnlockBits(data); 
+            dstimg.UnlockBits(data2);
+
+            pictureBox2.Image = dstimg;
+            
+            
+        }
+
+
+        private void xImageProcessGyogan(Bitmap srcimg, Bitmap dstimg)
+        {
+            //魚眼レンズ補正を行う
+            int w = srcimg.Width;
+            int h = srcimg.Height;
+
+            //srcimgに対して処理をして、picturebox2のbmpに格納する。
+
+            //            BitmapData data = srcimg.LockBits(new Rectangle(0, 0, srcimg.Width, srcimg.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            BitmapData data = srcimg.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData data2 = dstimg.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            byte[] buf = new byte[w * h * 3];    //RGBバッファを確保
+            byte[] buf2 = new byte[w * h * 3];    //RGBバッファを確保
+            Marshal.Copy(data.Scan0, buf, 0, buf.Length);   //bufに画像をコピー
+
+            double r = trackBar1.Value;
+            double D = trackBar2.Value;
+
+
+            //dstの(x,y)に対して、元画像srcのどの点が対応するか計算し、(x2,y2)とする。
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    double x1, y1, w1, h1 ;
+                    double x2, y2;
+
+                    x1 = (double)x;
+                    y1 = (double)y;
+                    w1 = (double)w;
+                    h1 = (double)h;
+
+                    x2 = r * (x1 - w1 / 2) / Math.Sqrt(D * D + (x1 - w1 / 2) * (x1 - w1 / 2) + (y1 - h1 / 2) * (y1 - h1 / 2)) + w / 2;
+                    y2 = r * (y1 - h1 / 2) / Math.Sqrt(D * D + (x1 - w1 / 2) * (x1 - w1 / 2) + (y1 - h1 / 2) * (y1 - h1 / 2)) + h / 2;
+                    Color c=BiLinearInterpolation(x2,y2,w,h,buf);   //元画像のx2,y2のいちのピクセル値を得る
+                    xSetPixel(x, y, c, w, h, buf2);
+                }
+            }
+
+
+            Marshal.Copy(buf2, 0, data2.Scan0, buf2.Length);
+            srcimg.UnlockBits(data);
+            dstimg.UnlockBits(data2);
+
+            pictureBox2.Image = dstimg;
+
+
+        }
+
+
+        public Color xGetPixel(int x, int y, int w, int h, byte[] buf)
+        {
+            byte r, g, b;
+            r = 0x80;
+            g = 0x80;
+            b = 0x80;
+           
+            if ((0 <= x) && (x < w) && (0 <= y) && (y < h))
+            {
+                r = buf[(x + y * w)*3 + 0];
+                g = buf[(x + y * w)*3 + 1];
+                b = buf[(x + y * w)*3 + 2];
+                
+            }
+            
+            return Color.FromArgb(r,g,b);
+        }
+
+        private Color BiLinearInterpolation(double x, double y, int w,int h,byte[] buf)
+        {
+            //バイリニア法で補完したピクセルを得る
+
+            int x1 = (int)Math.Floor(x);
+            int y1 = (int)Math.Floor(y);
+
+            double R = 0;
+            double G = 0;
+            double B = 0;
+
+            double[] Wx = new double[2];
+            double[] Wy = new double[2];
+            
+            Wx[0] = 1 - (x - x1);
+            Wx[1] = x - x1;
+            Wy[0] = 1 - (y - y1);
+            Wy[1] = y - y1;
+
+            for (int i = 0; i < Wx.Length; i++)
+            {
+                for (int j = 0; j < Wy.Length; j++)
+                {
+                    Color c = xGetPixel(x1 + i, y1 + j, w, h, buf);
+                    R += ((double)c.R * Wx[i] * Wy[j]);
+                    G += ((double)c.G * Wx[i] * Wy[j]);
+                    B += ((double)c.B * Wx[i] * Wy[j]);
+
+                }
+            }
+
+            return Color.FromArgb((int)R, (int)G, (int)B);
+        }
+
+        private void xSetPixel(int x, int y, Color color, int w, int h, byte[] buf)
+        {
+            byte r, g, b;
+            r = color.R;
+            g = color.G;
+            b = color.B;
+
+            if ((0 <= x) && (x < w) && (0 <= y) && (y < h))
+            {
+                buf[(x + y * w) * 3 + 0] = r;
+                buf[(x + y * w) * 3 + 1] = g;
+                buf[(x + y * w) * 3 + 2] = b;
+
+            }
+        }
+        private void xLog(String s)
+        {
+            //ログ出力
+            DateTime dt = DateTime.Now;
+            textBox1.AppendText(dt.ToString() + " " + s + "\n");
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(@"log.txt", true, System.Text.Encoding.GetEncoding("shift_jis"));
+            sw.Write(dt.ToString() + " " + s + "\r\n");
+            sw.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+            label2.Text = trackBar2.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);  //picturebox1から画像を読み込む
+            Bitmap bitmap2 = new Bitmap(bitmap.Width, bitmap.Height);
+            //                    xImageProcess(bitmap, bitmap2);
+            xImageProcessGyogan(bitmap, bitmap2);
+            //pictureBox2.Image = bitmap2;
+
+        }
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //フォームが閉じるときに現在の設置値を保存する
+            appSettings.r = trackBar1.Value;
+            appSettings.d = trackBar2.Value;
+            appSettings.save_check = checkBox1.Checked;
+
+            //＜XMLファイルに書き込む＞
+            //XmlSerializerオブジェクトを作成
+            //書き込むオブジェクトの型を指定する
+            System.Xml.Serialization.XmlSerializer serializer1 =
+                new System.Xml.Serialization.XmlSerializer(typeof(Settings));
+            //ファイルを開く（UTF-8 BOM無し）
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(
+                gSettingFn, false, new System.Text.UTF8Encoding(false));
+            //シリアル化し、XMLファイルに保存する
+            serializer1.Serialize(sw, appSettings);
+            //閉じる
+            sw.Close();
+        }
+    }
+}
